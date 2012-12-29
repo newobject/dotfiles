4653cd6350ee432d26adee2cc1c024a454e4c132
Bo Zhang <bozhang@watercooler-inc.com>
2012-12-29 23:04:32 +0800

    add oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,16 @@ echo "\nClone vundle..."
 git clone https://github.com/gmarik/vundle.git $DOTFILES/vim/bundle/vundle
 
 echo "\nLink files..."
+ln -s $DOTFILES/oh-my-zsh $HOME/.oh-my-zsh
+ln -s $DOTFILES/zshrc $HOME/.zshrc
 ln -s $DOTFILES/vimrc $HOME/.vimrc
 ln -s $DOTFILES/gvimrc $HOME/.gvimrc
 ln -s $DOTFILES/gvimrc.before $HOME/.gvimrc.before
 ln -s $DOTFILES/vim $HOME/.vim
 ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
 
+echo "\nChange to zsh"
+chsh -s /bin/zsh
 
 echo "\nInstalling plugins using vundle..."
 vi +BundleInstall! +BundleClean +q
